e300c7c258b88fcb59805154aca4e9165f8bec61
Meet Shah <meet1995@gmail.com>
2016-03-29 17:26:50 +0530

    Entry Screen finished

--- app/src/main/java/www/meeteor/me/popularmovies/MainActivity.java
+++ app/src/main/java/www/meeteor/me/popularmovies/MainActivity.java
@@ -1,31 +1,37 @@
 package www.meeteor.me.popularmovies;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private List<Movie> mMovieList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mMovieList= new ArrayList<>();
+        PosterTask posterTask = new PosterTask();
+        posterTask.execute();
     }
 
     @Override
@@ -48,5 +54,57 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class  PosterTask  extends AsyncTask<Void, Void, JSONArray> {
+
+        @Override
+        protected JSONArray doInBackground(Void... params) {
+
+            URL url;
+            try {
+                url = new URL("http://api.themoviedb.org/3/movie/popular?api_key=16bce96206689130d2690ca204b3a8b7");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.connect();
+                if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+                    String line;
+                    String response = "";
+                    while ((line = rd.readLine()) != null) {
+                        response += line;
+                    }
+                    JSONObject rObj = new JSONObject(response);
+                    return rObj.getJSONArray("results");
+                }
+
+            }  catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(JSONArray jsonArray) {
+            super.onPostExecute(jsonArray);
+            for (int i = 0;i<jsonArray.length();i++ ){
+                try {
+                    mMovieList.add(new Movie(jsonArray.getJSONObject(i)));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            RecyclerView posterRv = (RecyclerView) findViewById(R.id.poster_rv);
+            PosterRVAdapter posterRVAdapter = new PosterRVAdapter(mMovieList);
+            assert posterRv != null;
+            posterRv.setAdapter(posterRVAdapter);
+            StaggeredGridLayoutManager gridLayoutManager =
+                    new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+            posterRv.setLayoutManager(gridLayoutManager);
+        }
     }
 }
